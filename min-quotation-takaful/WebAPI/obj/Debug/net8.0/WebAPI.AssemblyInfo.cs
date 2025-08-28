8ba5ba63a2af76eefb3306587ba195d9ed0b2362
mr-indonesia <secretcode190@gmail.com>
2025-08-28 10:51:45 +0700

    update dll

--- min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b3bed2c39e01c64933f105b356975f13743988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0535c778483b3de533366ccd626d610f81b42d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
